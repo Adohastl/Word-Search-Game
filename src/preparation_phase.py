21fedb1625554fd2b1320777425ec804923b6009
adohastl <adohastl@gmail.com>
2025-04-12 18:22:54 +0330

    lastone

--- src/preparation_phase.py
+++ src/preparation_phase.py
@@ -5,21 +5,13 @@ def preparation(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         words = sorted((list(set(file.read().splitlines()))), key= len) #remove duplicate elements and sort by lenght
         
-        wordssorted = []
-        current_strike = []
-        current_length = len(words[0])
-
+        min_len = len(words[0])
+        max_len = len(words[-1])
+        word_dict = {length: [] for length in range(min_len, max_len + 1)}
+        
         for word in words:
-            if len(word) == current_length:
-                current_strike.append(word)
-            else:
-                if current_strike:
-                    wordssorted.append(current_strike)
-                current_strike = [word]
-                current_length = len(word)
-        if current_strike:
-            wordssorted.append(current_strike)
-
-    return wordssorted
-
+            word_dict[len(word)].append(word)
+        
+        return list(word_dict.values())
+    
 print(preparation("data\\categories\\test.txt"))
