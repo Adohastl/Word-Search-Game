033856e0a108c2eb357ec3a7ddd1c074e96ef460
adohastl <adohastl@gmail.com>
2025-04-12 17:45:32 +0330

    more changes

--- src/preparation_phase.py
+++ src/preparation_phase.py
@@ -1,23 +1,25 @@
 import os
 
-def preparation(file_path):
+def preparation(file_path): 
+
     with open(file_path, 'r', encoding='utf-8') as file:
-        words = file.read().splitlines()
-        cleanwords = []
-        for word in words:            
-            cleanwords.append(word.replace("آ", "ا"))
-    return(set(cleanwords))
+        words = sorted((list(set(file.read().splitlines()))), key= len) #remove duplicate elements and sort by lenght
+        
+        wordssorted = []
+        current_strike = []
+        current_length = len(words[0])
+
+        for word in words:
+            if len(word) == current_length:
+                current_strike.append(word)
+            else:
+                if current_strike:
+                    wordssorted.append(current_strike)
+                current_strike = [word]
+                current_length = len(word)
+        if current_strike:
+            wordssorted.append(current_strike)
+
+    return wordssorted
+
 print(preparation("data\\categories\\test.txt"))
-
-
-# def normalize_word(word):
-#     """Normalize words."""
-#     return word.replace('آ', 'ا')
-
-# def load_words_from_file(file_path):
-#     """Load words from a given file and normalize them."""
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         words = [normalize_word(line.strip()) for line in file if len(line.strip()) > 2]
-#     return words
-
-# print(load_words_from_file("data\\categories\\animals.txt"))
